ff3592a9f9373e5e721f913c883fbd473e5dd137
Antyradek <antyradek@gmail.com>
2016-05-05 23:23:42 +0200

    Sensor odbiera i wysyła

--- src/sensor/main.c
+++ src/sensor/main.c
@@ -14,6 +14,8 @@
 #include "../protocol.h"
 #include "../helper.h"
 
+#define BUF_SIZE 512
+
 int socket_prev;
 int socket_next;
 
@@ -24,42 +26,7 @@ const char* port_next;
 const char* addr_prev;
 const char* port_prev;
 
-//wysyłające
-struct sockaddr_in sockaddr_next;
-struct sockaddr_in sockaddr_prev;
-
-// Initializacja gniazd
-/*
-int initialize_sockets()
-{
-    socket_next = socket(AF_INET, SOCK_DGRAM, 0);
-    if(socket_next < 0)
-    {
-        print_error("Failed to create next sensor socket");
-        return -1;
-    }
-    socket_prev = socket(AF_INET, SOCK_DGRAM, 0);
-    if(socket_prev < 0)
-    {
-        print_error("Failed to create previous sensor socket");
-        return -1;
-    }
-
-	socket_in_from_next = socket(AF_INET, SOCK_DGRAM, 0);
-    if(socket_in_from_next < 0)
-    {
-        print_error("Failed to create input socket from next sensor");
-        return -1;
-    }
-
-	socket_in_from_prev = socket(AF_INET, SOCK_DGRAM, 0);
-    if(socket_in_from_prev < 0)
-    {
-        print_error("Failed to create input socket from previous sensor");
-        return -1;
-    }
-    return 0;
-}*/
+short sensor_id;
 
 //initializacja adresów
 int initilize_sockets()
@@ -71,7 +38,7 @@ int initilize_sockets()
 	int sfd;
 	//zmodyfikowany kod z przykładu w „man 3 getaddrinfo”
 
-	print_info("Now initializing passive socket to listen to previous sensor");
+	print_info("Now initializing passive socket to listen to previous sensor...");
 	memset(&hints, 0, sizeof(struct addrinfo));
 	//pasywny, do niego się łączymy, będzie użyty do bind
 	hints.ai_flags = AI_PASSIVE;
@@ -103,7 +70,7 @@ int initilize_sockets()
 		sfd = socket(rp -> ai_family, rp -> ai_socktype, rp -> ai_protocol);
 		if (sfd == -1)
 		{
-			print_info("One address failed");
+			print_info("One address failed.");
 			continue;
 		}
 		if (bind(sfd, rp -> ai_addr, rp -> ai_addrlen) == 0)
@@ -114,7 +81,7 @@ int initilize_sockets()
 	}
 	if (rp == NULL)
 	{
-		print_error("Could not bind");
+		print_error("Could not bind.");
 		return -1;
 	}
 	//cieknąca pamięć to zła rzecz
@@ -122,7 +89,7 @@ int initilize_sockets()
 	socket_prev = sfd;
 	print_success("Socket to previous sensor succeeded!");
 
-	print_info("Now initializing active socket to send data to next sensor");
+	print_info("Now initializing active socket to send data to next sensor...");
 	memset(&hints, 0, sizeof(struct addrinfo));
 	//aktywny, nim łączymy się do następnika
 	hints.ai_flags = 0;
@@ -148,7 +115,7 @@ int initilize_sockets()
 		sfd = socket(rp -> ai_family, rp -> ai_socktype, rp -> ai_protocol);
 		if (sfd == -1)
 		{
-			print_info("One address failed");
+			print_info("One address failed.");
 			continue;
 		}
 		if (connect(sfd, rp -> ai_addr, rp -> ai_addrlen) == 0)
@@ -159,7 +126,7 @@ int initilize_sockets()
 	}
 	if (rp == NULL)
 	{
-		print_error("Could not connect");
+		print_error("Could not connect.");
 		return -1;
 	}
 	socket_next = sfd;
@@ -172,19 +139,66 @@ int initilize_sockets()
 
 int main(int argc, char const *argv[])
 {
-	if(argc != 5)
+	if(argc != 6)
 	{
-		printf("Użycie: [adres_poprzednika] [port_poprzednika] [adres_następnika] [port_następnika] \n");
+		printf("Użycie: [adres_poprzednika] [port_poprzednika] [adres_następnika] [port_następnika] [id_czujnika]\n");
 		return 0;
 	}
 	addr_prev = argv[1];
 	port_prev = argv[2];
-
 	addr_next = argv[3];
 	port_next = argv[4];
-
+	sensor_id = (short)atoi(argv[5]);
 	initilize_sockets();
+	
+	char success_msg[50];
+	sprintf(success_msg, "Sensor %d is now ready for work.", sensor_id);
+	print_success(success_msg);
 
-	//TODO inicjalizacja aktywnych adresów do wysyłania
+	//główna pętla
+	struct sockaddr_storage peer_addr;
+    socklen_t peer_addr_len;
+    ssize_t nread;
+    char buf[BUF_SIZE];
+	int s;
+	while(1)
+	{
+		//odebranie pakietu
+		peer_addr_len = sizeof(struct sockaddr_storage);
+        nread = recvfrom(socket_prev, buf, BUF_SIZE, 0, (struct sockaddr *) &peer_addr, &peer_addr_len);
+        if (nread == -1)
+		{
+			print_info("Failed receive request.");
+			continue;
+		}
+		//sprawdzenie nadawcy pakietu
+       char host[NI_MAXHOST], service[NI_MAXSERV];
+       s = getnameinfo((struct sockaddr *) &peer_addr, peer_addr_len, host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICSERV);
+       if (s == 0)
+	   {
+		   char infotext[nread + 60];
+		   sprintf(infotext, "Received %ld bytes from %s:%s: %s\n", (long int) nread, host, service, buf);
+		   print_info(infotext);
+		   //TODO zabawa z odebranymi danymi
+
+		   //wysyłamy puki co testowo pakiet dalej
+		   char text_to_send[50];
+		   sprintf(text_to_send, "Dzień dobry od czujnika %d\n", sensor_id);
+		   int data_size = strlen(text_to_send) + 1;
+		   if(write(socket_next, text_to_send, data_size) != data_size)
+		   {
+			   print_info("Nie udało się wysłać pakietu");
+		   }
+		   else
+		   {
+			   print_info("Wysłano pakiet do następnika");
+		   }
+		   
+	   }
+    	else
+		{
+			print_error("getnameinfo() failed");
+		}
+	}
 
 }
