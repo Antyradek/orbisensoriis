92336739c4b9c5c14e4a5a146235fb3acef090c1
Mateusz <m.blicharski@stud.elka.pw.edu.pl>
2016-05-31 16:14:23 +0200

    Kosmetyczne zmiany w skrypcie testującym

--- test/tests.py
+++ test/tests.py
@@ -5,6 +5,7 @@
 
 import getopt
 import os
+import socket
 from subprocess import Popen, PIPE, DEVNULL
 import sys
 import time
@@ -13,23 +14,14 @@ import time
 config = {}
 
 
-def setup():
+def setup_sensors(sensors_no, start_port, last_port):
     """
-    Uruchamia podaną w pliku konfiguracyjnym lub za pomocą opcji -n liczbę sensorów oraz serwer
-    :return: (lista sensorów, server)
+    Tworzy senros_no czujników zaczynając od start_port i kończąc na last_port
+    :param sensors_no:
+    :param start_port:
+    :param last_port:
+    :return: lista uchytów na czujniki
     """
-    try:
-        sensors_no = int(config['SENSORS_NO'])
-    except KeyError:
-        print('[-] Number of sensors missing! (pass it by SENSORS_NO in conf file or -n option)')
-        sys.exit(1)
-
-    try:
-        server_path = config['SERVER_PATH']
-    except KeyError:
-        print('[-] Path to a server binary missing! (SERVER_PATH in conf file)')
-        sys.exit(1)
-
     try:
         sensor_path = config['SENSOR_PATH']
     except KeyError:
@@ -39,37 +31,68 @@ def setup():
     print('[*] Spawning ' + str(sensors_no) + ' sensors.')
 
     sensors = []
-    for i in range(0, sensors_no):
-        sensors.append(Popen([sensor_path, 'localhost', str(4001 + i), 'localhost', str(4002 + i), str(i)],
-                             stdin=DEVNULL, stdout=PIPE, stderr=DEVNULL))
+    for i in range(0, sensors_no - 1):
+        sensors.append(Popen([sensor_path, 'localhost', str(start_port + i), 'localhost', str(start_port + 1 + i),
+                              str(i)], stdin=DEVNULL, stdout=PIPE, stderr=DEVNULL))
+
+    sensors.append(Popen([sensor_path, 'localhost', str(start_port + sensors_no - 1),
+                          'localhost', str(last_port), str(sensors_no - 1)], stdin=DEVNULL, stdout=PIPE, stderr=DEVNULL))
+
+    return sensors
+
+
+def setup_server(first_port, last_port):
+    """
+    Uruchamia serwer z odpowiednim first_port i last_port
+    :param first_port: port wejściowy pierwszego czujnika
+    :param last_port: port wyjściowy ostatniego czujnika
+    :return: uchwyt na serwer
+    """
+    try:
+        server_path = config['SERVER_PATH']
+    except KeyError:
+        print('[-] Path to a server binary missing! (SERVER_PATH in conf file)')
+        sys.exit(1)
 
     print('[*] Starting server.')
-    server = Popen([server_path, '-l', str(4001 + sensors_no)], stdin=DEVNULL, stdout=PIPE, stderr=DEVNULL)
 
-    return sensors, server
+    server = Popen([server_path, '-f', 'localhost:' + str(first_port), '-l', str(last_port)],
+                   stdin=DEVNULL, stdout=PIPE, stderr=DEVNULL)
+
+    return server
 
 
 def test_normal():
-    print('[*] Testing normal work of a network.')
+    print('[*] Normal work test.')
 
-    sensors, server = setup()
+    try:
+        sensors_no = int(config['SENSORS_NO'])
+    except KeyError:
+        print('[-] Number of sensors missing! (pass it by SENSORS_NO in conf file or -n option)')
+        sys.exit(1)
+
+    sensors = setup_sensors(sensors_no, 4001, 4444)
+    server = setup_server(4001, 4444)
 
     for test_no in range(1, 11):
         print('[*] Test: ' + str(test_no))
         measurements = {}
-        for i in range(0, len(sensors)):
+        for i in range(0, sensors_no):
             line = sensors[i].stdout.readline()
             while b'measurement ' not in line:
                 line = sensors[i].stdout.readline()
             measurements[i] = int(line.split(b' ')[-1][:-2])
+
         line = server.stdout.readline()
         while not (b'Received' in line and b'measurements' in line):
             line = server.stdout.readline()
+
         received_no = int(line.split(b' ')[2])
-        if received_no != len(sensors):
+        if received_no != sensors_no:
             print('\033[-] Failed: server received ' + str(received_no) + ' measurements, ' +
-                  str(len(sensors)) + ' expected.\033[39m')
+                  str(sensors_no) + ' expected.\033[39m')
             continue
+
         received = {}
         for i in range(0, received_no):
             line = server.stdout.readline()
@@ -100,9 +123,16 @@ def test_normal():
 
 
 def test_emergency():
-    print('[*] Testing emergency mode.')
+    print('[*] Emergency mode test.')
 
-    sensors, server = setup()
+    try:
+        sensors_no = int(config['SENSORS_NO'])
+    except KeyError:
+        print('[-] Number of sensors missing! (pass it by SENSORS_NO in conf file or -n option)')
+        sys.exit(1)
+
+    sensors = setup_sensors(sensors_no, 4001, 4444)
+    server = setup_server(4001, 4444)
 
     print('[*] Killing first sensor... (it may take a while)')
     time.sleep(2)
@@ -121,19 +151,22 @@ def test_emergency():
     for test_no in range(1, 11):
         print('[*] Test: ' + str(test_no))
         measurements = {}
-        for i in range(1, len(sensors)):
+        for i in range(1, sensors_no):
             line = sensors[i].stdout.readline()
             while b'measurement ' not in line:
                 line = sensors[i].stdout.readline()
             measurements[i] = int(line.split(b' ')[-1][:-2])
+
         line = server.stdout.readline()
         while not (b'Received' in line and b'measurements' in line):
             line = server.stdout.readline()
+
         received_no = int(line.split(b' ')[2])
-        if received_no != len(sensors) - 1:
+        if received_no != sensors_no - 1:
             print('\033[31m[-] Failed: server received ' + str(received_no) + ' measurements, ' +
-                  str(len(sensors) - 1) + ' expected.\033[39m')
+                  str(sensors_no - 1) + ' expected.\033[39m')
             continue
+
         received = {}
         for i in range(0, received_no):
             line = server.stdout.readline()
@@ -162,7 +195,8 @@ def test_emergency():
         p.kill()
     server.kill()
 
-    sensors, server = setup()
+    sensors = setup_sensors(sensors_no, 4001, 4444)
+    server = setup_server(4001, 4444)
 
     print('[*] Killing last sensor... (it may take a while)')
     time.sleep(2)
@@ -181,19 +215,22 @@ def test_emergency():
     for test_no in range(1, 11):
         print('[*] Test: ' + str(test_no))
         measurements = {}
-        for i in range(0, len(sensors) - 1):
+        for i in range(0, sensors_no - 1):
             line = sensors[i].stdout.readline()
             while b'measurement ' not in line:
                 line = sensors[i].stdout.readline()
             measurements[i] = int(line.split(b' ')[-1][:-2])
+
         line = server.stdout.readline()
         while not (b'Received' in line and b'measurements' in line):
             line = server.stdout.readline()
+
         received_no = int(line.split(b' ')[2])
-        if received_no != len(sensors) - 1:
+        if received_no != sensors_no - 1:
             print('\033[31m[-] Failed: server received ' + str(received_no) + ' measurements, ' +
-                  str(len(sensors) - 1) + ' expected.\033[39m')
+                  str(sensors_no - 1) + ' expected.\033[39m')
             continue
+
         received = {}
         for i in range(0, received_no):
             line = server.stdout.readline()
@@ -222,10 +259,11 @@ def test_emergency():
         p.kill()
     server.kill()
 
-    if len(sensors) <= 2:
+    if sensors_no <= 2:
         return
 
-    sensors, server = setup()
+    sensors = setup_sensors(sensors_no, 4001, 4444)
+    server = setup_server(4001, 4444)
 
     print('[*] Killing middle sensor... (it may take a while)')
     killed_sensor_no = len(sensors) // 2
@@ -233,7 +271,7 @@ def test_emergency():
     sensors[killed_sensor_no].kill()
 
     # Czytamy dane z okresu normalnego działania sieci
-    for i in range(0, len(sensors)):
+    for i in range(0, sensors_no):
         if i == killed_sensor_no:
             continue
         line = sensors[i].stdout.readline()
@@ -247,7 +285,7 @@ def test_emergency():
     for test_no in range(1, 11):
         print('[*] Test: ' + str(test_no))
         measurements = {}
-        for i in range(0, len(sensors)):
+        for i in range(0, sensors_no):
             if i == killed_sensor_no:
                 continue
             line = sensors[i].stdout.readline()
@@ -271,7 +309,7 @@ def test_emergency():
             line = server.stdout.readline()
         received_no += int(line.split(b' ')[2])
 
-        if received_no != len(sensors) - 1:
+        if received_no != sensors_no - 1:
             print('\033[31m[-] Failed: server received ' + str(received_no) + ' measurements, ' +
                   str(len(sensors) - 1) + ' expected.\033[39m')
             continue
@@ -299,7 +337,7 @@ def test_emergency():
             for key in received:
                 print('    ' + str(key) + ': ' + str(received[key]))
 
-    for i in range(0, len(sensors)):
+    for i in range(0, sensors_no):
         if i == killed_sensor_no:
             continue
         sensors[i].kill()
@@ -326,7 +364,7 @@ def parse_config(file_name):
 # Mapowanie nazwy trybu testowania na funkcje testującą
 test_modes = {
     'normal': test_normal,
-    'emergency': test_emergency
+    'emergency': test_emergency,
 }
 
 
